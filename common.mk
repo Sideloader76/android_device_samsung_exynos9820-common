30782cf7db624714e9d5e16f867aa86a1fab48f4
Sideloader76 <155350390+Sideloader76@users.noreply.github.com>
2024-01-07 08:43:18 +0000

    common: add build flag to not build aperture camera

--- common.mk
+++ common.mk
@@ -358,4 +358,7 @@ PRODUCT_PACKAGES += \
 endif
 
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf      
+    $(COMMON_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf   
+
+#Alphadroid stuff
+PRODUCT_NO_CAMERA := true
